3e44f55fba82e6f2cfe3a74e808676ff72cbf66d
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2021-06-02 11:05:07 -0300

    Initial commit

---
+++ src/main/java/br/com/zuporange05/orangetalents05projetoproposta/OrangeTalents05ProjetoPropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zuporange05.orangetalents05projetoproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeTalents05ProjetoPropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeTalents05ProjetoPropostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zuporange05/orangetalents05projetoproposta/OrangeTalents05ProjetoPropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zuporange05.orangetalents05projetoproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeTalents05ProjetoPropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
