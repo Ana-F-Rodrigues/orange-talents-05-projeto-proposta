f88f766e3c649a3ec7fc803834ea3eacf0fbdb7e
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2021-06-10 19:14:09 -0300

    050.Login-via-senha

---
+++ src/main/java/br/com/zuporange05/orangetalents05projetoproposta/validacoes/SecurityConfigurations.java
@@ -0,0 +1,25 @@
+package br.com.zuporange05.orangetalents05projetoproposta.validacoes;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().httpStrictTransportSecurity().disable();
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(HttpMethod.POST, "/**").hasAuthority("SCOPE_meu-primeiro-escopo")
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    }
+
+}
